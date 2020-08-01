2ac08f6d9fa05816bb2593be0eb48d455ed67db2
zuiyu <wyx1818@vip.qq.com>
2020-08-01 14:38:02 +0800

    feat: 完成过滤组件

---
+++ src/components/Filter/style.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+import StyledText from '../Text/style'
+
+// 外层容器，用于包裹下拉菜单和label
+const Filters  = styled.div`
+
+`
+// 外层容器，用于包裹动作按钮和label
+const Action = styled.div`
+  justify-self: end;
+  ${StyledText} {
+    padding-right: 1rem;
+  }
+`
+
+// 过滤容器
+const StyledFilter = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  align-items: center;
+`
+
+export default StyledFilter
+
+export { Filters, Action }
