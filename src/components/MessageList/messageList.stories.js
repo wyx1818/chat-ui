5f08861baf40d3ad53828d11bc8869f8dd3682ff
zuiyu <wyx1818@vip.qq.com>
2020-08-01 16:09:33 +0800

    feat: 完成消息列表组件

---
+++ src/components/MessageList/messageList.stories.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+import MessageList from "."
+
+export default {
+  title: "页面组件/MessageList",
+  component: MessageList
+}
+
+export const Default = () => <MessageList />
---
+++ src/components/MessageList/style.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import StyledMessageCard from '../MessageCard/style'
+
+// 消息列表容器
+const ChartLIst = styled.div`
+  ${StyledMessageCard} {
+    margin-bottom: 20px;
+  };
+`
+
+const StyledMessageList = styled.div``
+
+export default StyledMessageList
+
+export { ChartLIst }
