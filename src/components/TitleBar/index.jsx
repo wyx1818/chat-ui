f8b2c2b75d7b3c1ddbd06220e337a3e026ac307a
zuiyu <wyx1818@vip.qq.com>
2020-08-01 16:48:42 +0800

    feat: 完成内容标题栏

---
+++ src/components/TitleBar/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import StyledTitleBar, { Actions, Title } from './style'
+import Avatar from '../Avatar'
+import Paragraph from '../Paragraph'
+import Text from '../Text'
+import Icon from '../Icon'
+
+import { ReactComponent as Call } from 'assets/icons/call.svg'
+import { ReactComponent as Camera } from 'assets/icons/camera.svg'
+import { ReactComponent as Options } from 'assets/icons/options.svg'
+
+function TitleBar ({
+  avatarSrc,
+  name,
+  status,
+  time,
+  children,
+  ...rest
+}) {
+  return (
+    <StyledTitleBar { ...rest }>
+      <Avatar src={ avatarSrc } status="online"/>
+      <Title>
+        <Paragraph size="large">{ name }</Paragraph>
+        <Paragraph type='secondary'>
+          <Text>{ status === 'online' ? '在线' : '离线' }</Text>
+          <Text> 最后阅读时间: { time }</Text>
+        </Paragraph>
+      </Title>
+      <Actions>
+        <Icon icon={ Call } opacity={0.3}/>
+        <Icon icon={Camera} opacity={0.3}/>
+        <Icon icon={Options} opacity={0.3}/>
+      </Actions>
+    </StyledTitleBar>
+  )
+}
+
+TitleBar.propTypes = {
+  children: PropTypes.any
+}
+
+export default TitleBar
---
+++ src/components/TitleBar/style.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+import StyledIcon from '../Icon/style'
+
+// 文字标题
+const Title = styled.div`
+  display: grid;
+  align-items: center;
+`
+
+// 可选动作
+const Actions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  ${StyledIcon} {
+    cursor: pointer;
+  }
+`
+
+const StyledTitleBar = styled.div`
+  display: grid;
+  grid-template-columns: 62px 1fr 112px;
+  padding: 30px;
+  max-height: 110px;
+  border-bottom: 1px solid ${ ({ theme }) => theme.grayBorder };
+`
+
+export default StyledTitleBar
+
+export { Title, Actions }
