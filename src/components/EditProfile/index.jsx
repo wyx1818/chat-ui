4f4742f669a33fc072e7e12106f1a32b5a97ce59
Zuiyu <wyx1818@vip.qq.com>
2020-08-09 15:12:28 +0800

    feat(src/components/editprofile): 完成个人资料编辑组件开发

--- src/components/EditProfile/index.jsx
+++ src/components/EditProfile/index.jsx
@@ -1,8 +1,30 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import 'styled-components/macro'
 
-import StyledEditProfile from './style'
+import StyledEditProfile, {
+  GenderAndRegion,
+  GroupTitle,
+  SelectGroup,
+  StyledIconInput,
+} from './style'
 import Profile from '../Profile'
+import Avatar from '../Avatar'
+import face from 'assets/images/face-male-2.jpg'
+import Button from '../Button'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import InputText from '../Input/InputText'
+import Radio from '../Radio'
+import LabelContainer from '../LabelContainer'
+import Select from '../Select'
+import Option from '../Option'
+import Icon from '../Icon'
+import {
+  faGithub,
+  faLinkedin,
+  faWeibo,
+} from '@fortawesome/free-brands-svg-icons'
 
 function EditProfile({ children, ...rest }) {
   // 控制编辑页面显隐
@@ -17,7 +39,72 @@ function EditProfile({ children, ...rest }) {
       />
     )
   }
-  return <StyledEditProfile {...rest}>{children}</StyledEditProfile>
+  return (
+    <StyledEditProfile {...rest}>
+      <Avatar
+        src={face}
+        size="160px"
+        css={`
+          grid-area: 1/1/2/2;
+          justify-self: center;
+          margin-bottom: 12px;
+        `}
+      />
+      <Button
+        diameter="52px"
+        css={`
+          grid-area: 1/1/3/2;
+          z-index: 10;
+          align-self: end;
+          justify-self: end;
+        `}
+        onClick={() => setShowEdit(false)}
+      >
+        <FontAwesomeIcon icon={faCheck} />
+      </Button>
+      <GroupTitle>基本信息</GroupTitle>
+      <InputText label="昵称" />
+      <GenderAndRegion>
+        <Radio.Group label="性别">
+          <Radio name="gender">男</Radio>
+          <Radio name="gender">女</Radio>
+        </Radio.Group>
+        <LabelContainer label="地区">
+          <SelectGroup>
+            <Select type="form">
+              <Option>省份</Option>
+            </Select>
+            <Select type="form">
+              <Option>城市</Option>
+            </Select>
+            <Select type="form">
+              <Option>区县</Option>
+            </Select>
+          </SelectGroup>
+        </LabelContainer>
+      </GenderAndRegion>
+      <InputText label="个性签名" />
+
+      <GroupTitle>联系信息</GroupTitle>
+      <InputText label="联系电话" />
+      <InputText label="电子邮箱" />
+      <InputText label="个人网站" />
+
+      <GroupTitle>社交信息</GroupTitle>
+      <IconInput icon={faWeibo} bgColor="#F06767" />
+      <IconInput icon={faGithub} bgColor="black" />
+      <IconInput icon={faLinkedin} bgColor="#2483C0" />
+    </StyledEditProfile>
+  )
+}
+
+function IconInput({ icon, bgColor, ...rest }) {
+  return (
+    <StyledIconInput>
+      <Icon.Social icon={icon} bgColor={bgColor} />
+      <InputText {...rest} />
+    </StyledIconInput>
+  )
 }
 
 EditProfile.propTypes = {
--- src/components/EditProfile/style.jsx
+++ src/components/EditProfile/style.jsx
@@ -1,5 +1,41 @@
 import styled from 'styled-components'
+import Text from '../Text'
 
-const StyledEditProfile = styled.div``
+// 分组名称
+const GroupTitle = styled(Text).attrs({ size: 'large' })`
+  align-self: end;
+`
+
+// 性别和地区
+const GenderAndRegion = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-content: space-between;
+`
+
+// 选择群组
+const SelectGroup = styled.div`
+  > * {
+    margin: 0 4px;
+  }
+  margin: 0 -4px;
+`
+
+const StyledIconInput = styled.div`
+  display: grid;
+  grid-template-columns: 38px 1fr;
+  align-items: end;
+`
+
+const StyledEditProfile = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  row-gap: 20px;
+  padding: 30px;
+  max-height: 100vh;
+  overflow-y: auto;
+`
 
 export default StyledEditProfile
+
+export { GroupTitle, GenderAndRegion, SelectGroup, StyledIconInput }
